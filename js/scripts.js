10602bcb3e0b01b6678d42e65b42d3949b36d0dc
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-02 19:54:26 -0700

    add mad-lib.html and associated code in js and css for form practice excercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* This is for index.html practice
 $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
@@ -39,5 +40,28 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("green-background");
 
+  });
+});
+*/
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
   });
 });
